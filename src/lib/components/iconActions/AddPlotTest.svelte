8c244e613fb622dc48e4a3c32981081c92de8c75
yz-329 <yuxingz0329@gmail.com>
2025-07-14 22:37:41 +1200

    Create test add plot modal for integration

--- src/lib/components/iconActions/AddPlotTest.svelte
+++ src/lib/components/iconActions/AddPlotTest.svelte
@@ -7,15 +7,14 @@
 	import Icon from '$lib/icons/Icon.svelte';
 	import Modal from '$lib/components/reusables/Modal.svelte';
 	import Dropdown from '$lib/components/reusables/Dropdown.svelte';
-	import { faL } from '@fortawesome/free-solid-svg-icons';
 
-	let { showDropdown = $bindable(false), dropdownTop = 0, dropdownLeft = 0 } = $props();
+	let { showModal = false } = $props();
 
-	let showModal = $state(false);
 	let plotType = $state();
 	let plotName = $derived.by(() => {
 		return plotType + '_' + Math.round(Math.random() * 10, 2);
 	});
+
 	function openModal(type) {
 		showModal = true;
 		plotType = type;
@@ -23,62 +22,19 @@
 
 	let xCol = $state();
 	let yCol = $state();
+
 	function confirmImport() {
 		const newPlot = new Plot({ name: plotName, type: plotType });
-		// let xId = getColumnById(xCol);
-		// let yId = getColumnById(yCol);
-
-		console.log("DEBUG: x and y id = ", xCol.id, yCol.id);
-
 		newPlot.plot.addData( {
 			x: {refId: xCol},
 			y: {refId: yCol}
 		});
-
-		console.log(newPlot);
-
 		pushObj(newPlot);
 
 		showModal = false;
-		showDropdown = false;
 	}
+
 </script>
-
-<Dropdown bind:showDropdown top={dropdownTop} left={dropdownLeft}>
-	{#snippet groups()}
-		<div class="action">
-			<button
-				onclick={() => {
-					openModal('actogram');
-				}}
-			>
-				Create New Actogram
-			</button>
-		</div>
-
-		<div class="action">
-			<button
-				onclick={() => {
-					openModal('periodogram');
-				}}
-			>
-				Create New Periodogram
-			</button>
-		</div>
-
-		<div class="action">
-			<button
-				onclick={() => {
-					openModal('scatterplot');
-				}}
-			>
-				Create New ScatterPlot
-			</button>
-		</div>
-	{/snippet}
-</Dropdown>
-
-<!-- TODO: change modal component to icon-like structure? -->
 
 <Modal bind:showModal>
 	{#snippet header()}
@@ -141,82 +97,3 @@
 		</div>
 	{/snippet}
 </Modal>
-
-<style>
-	.action button {
-		margin: 0.6em;
-		font-size: 14px;
-	}
-
-	.action:hover {
-		background-color: var(--color-lightness-95);
-	}
-
-	button {
-		background-color: transparent;
-		border: none;
-		text-align: inherit;
-		font: inherit;
-		border-radius: 0;
-		appearance: none;
-
-		cursor: pointer;
-	}
-
-	.heading {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.choose-file-container {
-		height: 2em;
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		gap: 1rem;
-	}
-
-	.choose-file-button {
-		background-color: var(--color-lightness-95);
-		padding: 8px 12px;
-		border-radius: 4px;
-
-		font-size: 14px;
-		text-align: center;
-	}
-
-	.choose-file-button:hover {
-		background-color: var(--color-hover);
-	}
-
-	.preview-placeholder {
-		width: 100%;
-		min-height: 100px;
-	}
-
-	.selected-preview {
-		color: var(--color-lightness-35);
-		font-size: 14px;
-	}
-
-	.import-button-container {
-		display: flex;
-		justify-content: flex-end;
-		/* margin-right: 1rem; */
-	}
-
-	.import-button {
-		margin-top: 10px;
-		background-color: var(--color-lightness-95);
-		border-radius: 4px;
-		padding: 10px;
-		padding-right: 12px;
-
-		font-size: 14px;
-		text-align: center;
-	}
-
-	.import-button:hover {
-		background-color: var(--color-hover);
-	}
-</style>
