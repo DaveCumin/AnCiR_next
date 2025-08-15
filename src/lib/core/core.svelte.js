4664d69e7c8725d66c21b729550b04b8594141f7
yz-329 <yuxingz0329@gmail.com>
2025-08-15 13:17:27 +1200

    RELEASE TO THE WORLD version β.4.3

--- src/lib/core/core.svelte.js
+++ src/lib/core/core.svelte.js
@@ -44,7 +44,7 @@ export const appState = $state({
 });
 
 export const appConsts = $state({
-	version: 'β.4.2',
+	version: 'β.4.3',
 	processMap: new Map(),
 	plotMap: new Map(),
 	tableProcessMap: new Map(),
