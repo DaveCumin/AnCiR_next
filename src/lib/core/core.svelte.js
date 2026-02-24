20efd2825620fd1c426f2c77801375b3361febf0
David Cumin <david.cumin@gmail.com>
2026-02-24 21:49:27 +1300

    merged with show peaks

--- src/lib/core/core.svelte.js
+++ src/lib/core/core.svelte.js
@@ -59,7 +59,7 @@ export const appState = $state({
 });
 
 export const appConsts = $state({
-	version: 'β.10.4',
+	version: 'β.10.5',
 	processMap: new Map(),
 	plotMap: new Map(),
 	tableProcessMap: new Map(),
