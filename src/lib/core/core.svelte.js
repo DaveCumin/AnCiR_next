264c3b3b543b6794ab895409ab138f37eed4201c
David Cumin <david.cumin@gmail.com>
2026-03-30 12:57:48 +1300

    .

--- src/lib/core/core.svelte.js
+++ src/lib/core/core.svelte.js
@@ -60,7 +60,7 @@ export const appState = $state({
 });
 
 export const appConsts = $state({
-	version: 'β.11.2',
+	version: 'β.11.3',
 	processMap: new Map(),
 	plotMap: new Map(),
 	tableProcessMap: new Map(),
