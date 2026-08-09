9a3849af11d7b44ee391e12d71adcd1fcfe3daa5
David Cumin <david.cumin@gmail.com>
2026-08-10 11:41:32 +1200

    v72.18; updated colours/shades to be more accessible.

--- src/lib/core/core.svelte.js
+++ src/lib/core/core.svelte.js
@@ -447,7 +447,7 @@ export const appState = $state({
 });
 
 export const appConsts = $state({
-	version: 'β.72.17',
+	version: 'β.72.18',
 	processMap: new Map(),
 	plotMap: new Map(),
 	tableProcessMap: new Map(),
